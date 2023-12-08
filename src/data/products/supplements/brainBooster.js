18c258ae85e7fba8136579adf4b38bf58392a85c
TRICIATEOhs <tricia.ths@hotmail.com>
2023-12-09 05:35:42 +0800

    testing: product images not showing

--- src/data/products/supplements/brainBooster.js
+++ src/data/products/supplements/brainBooster.js
@@ -2,7 +2,7 @@ const brainBooster = [
     {
         "id":1, 
         "name":"Cavinton", 
-        "image": require("../../../images/products/supplementsBrainBoosters/cavinton.jpg"),
+        "image": require("/images/products/supplementsBrainBoosters/cavinton.jpg"),
         "description": "Cerebral circulatory disorders of various origin. Hypertensive encephalopathy. Intermittent vascular cerebral insufficiency. Improvement of collateral circulation in ischaemic cerebral injuries and in advanced cerebral arteriosclerosis.",
         "packSize": "5mg x 50’s tablets",
         "productOf": "Hungary",
